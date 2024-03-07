55d26cb2114d3f20e9f67d48ee57db066043ad94
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-07 21:37:18 +0800

    📦 new: add transaction command

--- source/utils/handleCommand.js
+++ source/utils/handleCommand.js
@@ -1,4 +1,5 @@
 import address from '../commands/address.js';
+import transaction from '../commands/transaction.js';
 import openURL from './handleOpenURL.js';
 
 /**
@@ -9,8 +10,13 @@ import openURL from './handleOpenURL.js';
  * @description Handle the command and call the function for the specific command.
  */
 const handleCommand = (command, chain, input, flags) => {
-  if (command === 'address' || command === 'contract') {
+  //handle address and contract command
+  if (command === 'address' || command === 'a' || command === 'contract' || command === 'c') {
     const url = address(chain, input, flags);
+    openURL(url);
+  //handle transaction command
+  } else if (command === 'transaction' || command === 'tx') {
+    const url = transaction(chain, input, flags);
     openURL(url);
   } else {
     console.error('[xplorescan] Command not found. Please use "xplorescan --help" to see the available commands.');
